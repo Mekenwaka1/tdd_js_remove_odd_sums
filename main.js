59788afa75e62220902d66126326b7241b619147
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-08-29 09:44:04 -0400

    Created final exercise

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function removeOddSum() {
+function removeOddSum(listOfObjects, str1, str2) {
+  const result = listOfObjects.filter(obj => (obj["a"] + obj["b"]) % 2 === 0);
+  return result;
 }
 
+removeOddSum(
+  [{a: 5, b: 5}, {a: 3, b: 4}, {a: 2, b: 0}, {a: 2, b: 1}],
+  "a",
+  "b"
+);
 module.exports = removeOddSum;
